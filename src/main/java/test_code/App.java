76de163b39d0abc5bd2ae2a8d84d9f1048c3dfb4
NKV <babu6n75@gmail.com>
2020-11-04 10:22:36 +0530

    Task maven project created and push to CP

---
+++ src/main/java/test_code/App.java
@@ -0,0 +1,13 @@
+package test_code;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
